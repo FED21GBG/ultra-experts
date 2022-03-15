c669ed5673f2c042df908db117d88f2acb964450
VilmaKristina <v.pakstyte@gmail.com>
2022-03-15 12:03:23 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Good evening!</h1>
-      <h2>Good night!</h2>
-      <h3>night!</h3>
     </div>
   );
 }
